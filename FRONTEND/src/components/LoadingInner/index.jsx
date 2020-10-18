75f79cc4d0c0531db9c73311166e35248cead973
Maycow Moura <maycowmoura@gmail.com>
2020-10-18 17:30:50 -0300

    feat: added size and color

--- FRONTEND/src/components/LoadingInner/index.jsx
+++ FRONTEND/src/components/LoadingInner/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './style.scss';
 import { AiOutlineLoading3Quarters as Loader } from 'react-icons/ai';
 
-export default function LoadingInner({ text }) {
+export default function LoadingInner({ text, loaderSize, loaderColor }) {
   return (
     <div id="loading-inner">
-      <Loader />
-      <span>{text}</span>
+      <Loader size={loaderSize} color={loaderColor} />
+      
+      {text && <span>{text}</span>}
     </div>
   )
 }
