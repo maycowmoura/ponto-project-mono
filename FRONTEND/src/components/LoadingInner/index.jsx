b68402ef9d4ac646f4916a09283fe5ea9ab2b09e
Maycow Moura <maycowmoura@gmail.com>
2020-10-18 23:42:40 -0300

    feat: added fixed attr

--- FRONTEND/src/components/LoadingInner/index.jsx
+++ FRONTEND/src/components/LoadingInner/index.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './style.scss';
 import { AiOutlineLoading3Quarters as Loader } from 'react-icons/ai';
 
-export default function LoadingInner({ text, loaderSize, loaderColor }) {
+export default function LoadingInner({ text, fixed, loaderSize, loaderColor }) {
+  
+  useEffect(() => {
+    if(!fixed) return;
+
+    document.body.classList.add('no-scroll', 'no-events');
+    return () => document.body.classList.remove('no-scroll', 'no-events');
+  }, [])
+
+
   return (
-    <div id="loading-inner">
+    <div id="loading-inner" className={fixed ? 'fixed' : ''} onClick={e => e.stopPropagation()}>
       <Loader size={loaderSize} color={loaderColor} />
       
       {text && <span>{text}</span>}
