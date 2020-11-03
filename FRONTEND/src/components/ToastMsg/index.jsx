d773fbd0e75e669b9ea94c700b4f229ec5fa8118
Maycow Moura <maycowmoura@gmail.com>
2020-11-03 20:50:01 -0300

    feat: added aboveAll atribute

--- FRONTEND/src/components/ToastMsg/index.jsx
+++ FRONTEND/src/components/ToastMsg/index.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import './style.scss';
 import { FiAlertTriangle, FiHelpCircle } from 'react-icons/fi';
 
-export default function ToastMsg({ text, info, close }) {
+export default function ToastMsg({ text, info, close, aboveAll }) {
 
   return (
-    <div id="toast-msg" className={info ? 'info' : ''} onClick={() => close && close()}>
+    <div
+      id="toast-msg"
+      className={info ? 'info' : '' + aboveAll ? ' above-all' : ''}
+      onClick={() => close && close()}
+    >
       {info ? <FiHelpCircle /> : <FiAlertTriangle />}
       <div>{text}</div>
     </div>
