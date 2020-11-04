bad0261aed8740f7f6eb43c996a49a0a9499023f
Maycow Moura <maycowmoura@gmail.com>
2020-11-03 22:17:33 -0300

    fix: small bug with above-all class

--- FRONTEND/src/components/ToastMsg/index.jsx
+++ FRONTEND/src/components/ToastMsg/index.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './style.scss';
 import { FiAlertTriangle, FiHelpCircle } from 'react-icons/fi';
 
-export default function ToastMsg({ text, info, close, aboveAll }) {
+export default function ToastMsg({ text, info, close, aboveAll}) {
 
   return (
     <div
       id="toast-msg"
-      className={info ? 'info' : '' + aboveAll ? ' above-all' : ''}
+      className={(info ? 'info' : '') + (aboveAll ? ' above-all' : '')}
       onClick={() => close && close()}
     >
       {info ? <FiHelpCircle /> : <FiAlertTriangle />}
