ac27a593af96c0c62bef867314e98ae43ea99ba2
Maycow Moura <maycowmoura@gmail.com>
2020-11-12 15:18:28 -0300

    feat: employers transfer log

--- BACKEND/controllers/employers/transfer-employer.php
+++ BACKEND/controllers/employers/transfer-employer.php
@@ -22,50 +22,54 @@ require_once __DIR__ . '/../../models/SQL.php';
 use Respect\Validation\Validator as v;
 
 
-$auth = new Auth();
-$auth->mustBeAdmin();
-$client = $auth->client;
-$accessibleEmployers = $auth->getAccessibleEmployers();
-
-if (!in_array($employerId, $accessibleEmployers)) {
-  die('{"error": "Você não tem acesso a esse funcionário."}');
-}
-
-
-
 try {
   v::key('place', v::numericVal())->check(PUT);
 } catch (Exception $e) {
-  die(_json_encode([
-    'error' => $e->getMessage()
-  ]));
+  error($e->getMessage());
 }
 
 
-$place = PUT['place'];
+$auth = new Auth();
+$auth->mustBeAdmin();
+$userId = $auth->userId;
+$client = $auth->client;
+$accessiblePlaces = $auth->getAccessiblePlaces();
+$accessibleEmployers = $auth->getAccessibleEmployers();
+$to_place = PUT['place'];
+$date = date('Y-m-d');
+$time = time();
+
+
+if (!in_array($to_place, $accessiblePlaces)) {
+  error("Você não tem acesso a esse local.");
+}
+
+if (!in_array($employerId, $accessibleEmployers)) {
+  error("Você não tem acesso a esse funcionário.");
+}
 
 try {
   $sql = new SQL();
+  $sql->beginTransaction();
   $sql->execute(
-   "UPDATE `$client-employers`
-    SET place = '$place'
+    "SELECT @from_place := place FROM `$client-employers` WHERE id = '$employerId'"
+  );
+  $sql->execute(
+    "INSERT INTO `$client-employers-transfers` 
+    (date, employer_id, from_place, to_place, transfered_by, transfered_at) 
+    VALUES ('$date', '$employerId', @from_place, '$to_place', '$userId', '$time')"
+  );
+  $sql->execute(
+    "UPDATE `$client-employers`
+    SET place = '$to_place'
     WHERE id = '$employerId'"
   );
+  $sql->commit();
 
 } catch (Exception $e) {
-  $message = $e->getMessage();
-  $message = strpos($message, ' 23000 ') > 0 ? 'Local de destino não encontrado.' : $message;
-
-  die(_json_encode([
-    'error' => $message
-  ]));
+  error($e->getMessage());
 }
 
-
-
-if ($sql->getTotalAffected() == 0) {
-  die('{"error": "Nenhum funcionário afetado."}');
-}
 
 
 die('{"ok": true}');
