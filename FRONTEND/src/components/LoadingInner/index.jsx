fa5ffa35c84be8222ed642ffe4a7c5989a1ba3d2
Maycow Moura <maycowmoura@gmail.com>
2020-11-13 02:48:37 -0300

    feat: error param for loading

--- FRONTEND/src/components/LoadingInner/index.jsx
+++ FRONTEND/src/components/LoadingInner/index.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 import './style.scss';
 import { AiOutlineLoading3Quarters as Loader } from 'react-icons/ai';
+import { RiErrorWarningLine as Warn } from 'react-icons/ri';
 
-export default function LoadingInner({ text, fixed, loaderSize, loaderColor }) {
-  
+export default function LoadingInner({ text, fixed, loaderSize, loaderColor, error = false }) {
+
   useEffect(() => {
-    if(!fixed) return;
+    if (!fixed) return;
 
     document.body.classList.add('no-scroll', 'no-events');
     return () => document.body.classList.remove('no-scroll', 'no-events');
@@ -13,9 +14,15 @@ export default function LoadingInner({ text, fixed, loaderSize, loaderColor }) {
 
 
   return (
-    <div id="loading-inner" className={fixed ? 'fixed' : ''} onClick={e => e.stopPropagation()}>
-      <Loader size={loaderSize} color={loaderColor} />
-      
+    <div id="loading-inner"
+      className={(fixed ? 'fixed' : '') + (error ? ' error' : '')}
+      onClick={e => e.stopPropagation()}
+    >
+      {!error
+        ? <Loader size={loaderSize} color={loaderColor} />
+        : <Warn />
+      }
+
       {text && <span>{text}</span>}
     </div>
   )
