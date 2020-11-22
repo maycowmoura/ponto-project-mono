50bd1f2cd7ac946524c5fc15e0c551fd47350b3f
Maycow Moura <maycowmoura@gmail.com>
2020-11-22 16:59:04 -0300

    feat: added worked days and weekend days

--- FRONTEND/src/pages/Marks/ListMarks/EmployerCard/Calculations/index.jsx
+++ FRONTEND/src/pages/Marks/ListMarks/EmployerCard/Calculations/index.jsx
@@ -11,8 +11,11 @@ function Calculations({ marks }) {
     extraHours: 0,
     late: 0,
     earlyExit: 0,
+    workedDays: 0,
     saturdays: 0,
-    sundays: 0
+    saturdaysHours: 0,
+    sundays: 0,
+    sundaysHours: 0
   };
 
   function sum(key, condition, value){
@@ -28,19 +31,35 @@ function Calculations({ marks }) {
     
     const balance = parseInt(item.time_out) - parseInt(item.time_in) - lunch_time;
     sum('misses', item.time_in < 0, 1);
+
     sum('extraHours', item.time_before > 0, item.time_before);
     sum('extraHours', item.time_after > 0, item.time_after);
+
     sum('late', item.time_before < 0, item.time_before);
     sum('earlyExit', item.time_after < 0, item.time_after);
-    sum('saturdays', (item.weekday == 6 && item.time_in), balance);
-    sum('sundays', (item.weekday == 0 || !!item.holiday), balance);
+
+    sum('workedDays', (item.time_in > -1), 1);
+    sum('saturdays', (item.weekday == 6 && item.time_in > -1), 1);
+    sum('sundays', (item.weekday == 0 || !!item.holiday), 1);
+
+    sum('saturdaysHours', (item.weekday == 6 && item.time_in > -1), balance);
+    sum('sundaysHours', (item.weekday == 6 && item.time_in > -1), balance);
   });
+
+  const workedDaysText = reducer.workedDays ? `${reducer.workedDays} dia${reducer.workedDays > 1 ? 's' : ''}` : 'Nenhum dia';
+  const missesText = reducer.misses ? `${reducer.misses} dia${reducer.misses > 1 ? 's' : ''}` : 'Nenhum dia';
+  const saturdayText = reducer.saturdays ? `${format(reducer.saturdaysHours)} (${reducer.saturdays} dia${reducer.saturdays > 1 ? 's' : ''})` : 'Nenhum dia';
+  const sundayText = reducer.sundays ? `${format(reducer.sundaysHours)} (${reducer.sundays} dia${reducer.sundays > 1 ? 's' : ''})` : 'Nenhum dia';
+
 
 
   return (
     <div className="calculations">
+      <div className="green">
+        Trabalhados: <span>{workedDaysText}</span>
+      </div>
       <div className="red">
-        Faltas: <span>{reducer.misses} dia(s)</span>
+        Faltas: <span>{missesText}</span>
       </div>
       <div className="green">
         Horas Extras: <span>{format(reducer.extraHours)}</span>
@@ -49,13 +68,13 @@ function Calculations({ marks }) {
         Atraso: <span>{format(Math.abs(reducer.late))}</span>
       </div>
       <div className="green">
-        Sábados: <span>{format(reducer.saturdays)}</span>
+        Sábados: <span>{saturdayText}</span>
       </div>
       <div className="red">
         Saída Cedo: <span>{format(Math.abs(reducer.earlyExit))}</span>
       </div>
       <div className="green">
-        Dom. e Fer.: <span>{format(reducer.sundays)}</span>
+        Dom. e Feriados: <span>{sundayText}</span>
       </div>
     </div>
   )
