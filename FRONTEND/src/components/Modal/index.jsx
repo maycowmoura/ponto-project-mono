38a8ba2caa667de2fcd2cf7c7706a689202d124d
Maycow Moura <maycowmoura@gmail.com>
2020-11-23 00:27:07 -0300

    feat: Modal component

---
+++ FRONTEND/src/components/Modal/index.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import './style.scss';
+import { useHistory } from 'react-router-dom';
+
+
+export default function Modal({ children, close = ()=>{} }) {
+  const history = useHistory();
+
+  useEffect(() => {
+    history.push('#modal');
+    document.body.classList.add('no-scroll');
+
+    const handleHashChange = () => window.location.hash !== 'modal' && close();
+    setTimeout(() => window.addEventListener('hashchange', handleHashChange), 10);
+
+    return () => {
+      window.location.hash === 'modal' && window.history.back();
+      document.body.classList.remove('no-scroll');
+      window.removeEventListener('hashchange', handleHashChange);
+    }
+  }, [])
+
+
+  return (
+    <div id="modal" onClick={() => close && close()}>
+      <div id="modal-inner" onClick={e => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>
+  )
+}
