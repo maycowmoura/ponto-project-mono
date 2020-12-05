65eb7d73a5c9816b6768d843fef74923ec0e0527
Maycow Moura <maycowmoura@gmail.com>
2020-12-05 12:47:59 -0300

    refactor: moved all routes to Router.jsx

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,21 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.scss';
 import { MainContextProvider, useMainContext } from './contexts/MainContext';
-import { MarksContextProvider } from './contexts/MarksContext';
-import Loading from './pages/Loading';
-import Login from './pages/Login';
-import Help from './pages/Help';
-import SetMarks from './pages/Marks/SetMarks';
-import Typing from './pages/Marks/SetMarks/Typing';
-import Commenting from './pages/Marks/SetMarks/Commenting';
-import Calendar from './pages/Marks/SetMarks/Calendar';
-import ListMarks from './pages/Marks/ListMarks';
-import Dashboard from './pages/Dashboard';
-import Places from './pages/Dashboard/Places';
-import EmployersPage from './pages/Dashboard/EmployersPage';
-import NewEmployer from './pages/Dashboard/EmployersPage/NewEmployer';
-import ClosePoint from './pages/Dashboard/ClosePoint';
+import Router from './Router';
 
 
 document.addEventListener('touchmove', e => {
@@ -33,31 +19,5 @@ export default function App() {
     window.history.pushState(null, null, homepage);
   }
 
-
-  return (
-    <MainContextProvider>
-      <BrowserRouter basename={homepage}>
-        <Switch>
-          <Route path="/" exact component={Loading} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/login/:temp_hash" component={Login} />
-          <Route path="/help" component={Help} />
-
-          <MarksContextProvider>
-            <Route path="/marks/list" component={ListMarks} />
-            <Route path="/marks/set" exact component={SetMarks} />
-            <Route path="/marks/set/typing" component={Typing} />
-            <Route path="/marks/set/commenting" component={Commenting} />
-            <Route path="/marks/set/calendar" component={Calendar} />
-
-            <Route path="/dashboard" exact component={Dashboard} />
-            <Route path="/dashboard/places" component={Places} />
-            <Route path="/dashboard/employers" exact component={EmployersPage} />
-            <Route path="/dashboard/employers/new" component={NewEmployer} />
-            <Route path="/dashboard/close-point" component={ClosePoint} />
-          </MarksContextProvider>
-        </Switch>
-      </BrowserRouter>
-    </MainContextProvider>
-  );
+  return <Router homepage={homepage} />
 }
